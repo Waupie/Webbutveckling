2c5ec6f5e11ed4160a1a19bcf1d70c1ee02a2121
Waupie <waup.max@gmail.com>
2019-11-03 17:41:59 +0100

    Finished 2.3.1

---
+++ 2 Serversidan: PHP/2.3 Information från användaren/2.3.1 Information sänd via adressfält och länkar/exempel.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2.3.1</title>
+</head>
+<body>
+    <p>Hej <?php echo $_GET['name']; ?></p>
+    <p>Din e-mail är <?php echo $_GET['email']; ?></p>
+</body>
+</html>
